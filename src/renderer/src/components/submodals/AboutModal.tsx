bf6744231727a703b9813dc5c562d7fd3a422cbf
Zean Isnomo <zeanfender11@gmail.com>
2026-07-29 12:10:21 +0700

    Add licenses

--- src/renderer/src/components/submodals/AboutModal.tsx
+++ src/renderer/src/components/submodals/AboutModal.tsx
@@ -1,4 +1,7 @@
 import CompassCADLogo from '../../assets/branding/logo-wordmark.svg'
+import { openModal } from '../ModalProvider'
+import OpenSourceLicensesLogo from '../../assets/icons/oss.svg'
+import OSLModal from './OSLModal'
 
 export default function AboutModal(): React.ReactElement {
   return (
@@ -8,6 +11,13 @@ export default function AboutModal(): React.ReactElement {
       <div>
         <p>CompassCAD NEXT</p>
         <p>Copyright &copy; 2024 - {new Date().getFullYear()} PT Trivi Buat Teknologi & zeankun.</p>
+      </div>
+      <br />
+      <div>
+        <button onClick={() => openModal('Open Source Licences      ', <OSLModal />)}>
+          <img src={OpenSourceLicensesLogo} height={24 } />
+          Open Source Licenses
+        </button>
       </div>
     </div>
   )
