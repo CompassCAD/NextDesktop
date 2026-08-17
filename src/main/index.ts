bcc29e7d40c2535f68ae4e27b3e4e03b233565ec
Zean Isnomo <zeanfender11@gmail.com>
2026-08-17 12:12:44 +0700

    Fix inversion of releases

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,7 +9,7 @@ import * as fs from 'fs'; // <- Prone to breaking ig
 
 autoUpdater.logger = log;
 (autoUpdater.logger as typeof log).transports.file.level = 'info';
-autoUpdater.forceDevUpdateConfig = app.isPackaged ? true : false;
+autoUpdater.forceDevUpdateConfig = !app.isPackaged;
 
 // WIP: adding user consent to download updates
 autoUpdater.autoDownload = false;
